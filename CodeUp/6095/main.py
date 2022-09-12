48c4838f63918a6215cde1d5e5d13a6ea5ddab72
Zerohertz <ohg3417@gmail.com>
2022-09-12 22:34:53 +0900

    Add CodeUp 6095

---
+++ CodeUp/6095/main.py
@@ -0,0 +1,18 @@
+n = int(input())
+x = []
+y = []
+
+for i in range(n):
+    a, b = input().split()
+    x.append(int(a)-1)
+    y.append(int(b)-1)
+
+d = [[0 for j in range(19)] for i in range(19)]
+
+for i in range(n):
+    d[x[i]][y[i]] = 1
+
+for i in range(19):
+    for j in range(19):
+        print(d[i][j], end = ' ')
+    print()
