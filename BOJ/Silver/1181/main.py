c6ce7292f28a5119ac498c188a1be09422fd02c8
zerohertz <ohg3417@gmail.com>
2022-09-16 10:43:15 +0900

    Add BOJ 1181

---
+++ BOJ/Silver/1181/main.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+L = []
+
+for i in range(N):
+  L.append(input())
+
+l = []
+
+for i in set(L):
+  l.append([i, len(i)])
+
+l.sort(key = lambda x: (x[1], x[0]))
+
+for i in range(len(l)):
+  print(l[i][0])
