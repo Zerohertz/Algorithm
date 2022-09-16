2358c833791a3de099274bdf5583abc18a6a1dce
zerohertz <ohg3417@gmail.com>
2022-09-16 11:34:14 +0900

    Add BOJ 10828

---
+++ BOJ/Silver/10828/Legacy/ver1.py
@@ -0,0 +1,25 @@
+N = int(input())
+stack = []
+
+for _ in range(N):
+  l = input().split()
+  if l[0] == 'push':
+    stack.append(int(l[1]))
+  elif l[0] == 'pop':
+    if len(stack) > 0:
+      print(stack[-1])
+      del stack[-1]
+    else:
+      print(-1)
+  elif l[0] == 'size':
+    print(len(stack))
+  elif l[0] == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif l[0] == 'top':
+    if len(stack) > 0:
+      print(stack[-1])
+    else:
+      print(-1)
---
+++ BOJ/Silver/10828/main.py
@@ -0,0 +1,26 @@
+import sys
+
+N = int(sys.stdin.readline())
+stack = []
+
+for _ in range(N):
+  l = sys.stdin.readline().split()
+  if l[0] == 'push':
+    stack.append(int(l[1]))
+  elif l[0] == 'pop':
+    if len(stack) > 0:
+      print(stack.pop())
+    else:
+      print(-1)
+  elif l[0] == 'size':
+    print(len(stack))
+  elif l[0] == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif l[0] == 'top':
+    if len(stack) > 0:
+      print(stack[-1])
+    else:
+      print(-1)
