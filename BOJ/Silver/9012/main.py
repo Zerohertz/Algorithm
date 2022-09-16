bb608f1a537b410e6a0eed6b67c10751ef9725c2
Zerohertz <ohg3417@gmail.com>
2022-09-16 17:09:58 +0900

    Add BOJ 9012

---
+++ BOJ/Silver/9012/main.py
@@ -0,0 +1,15 @@
+N = int(input())
+for _ in range(N):
+  S = input()
+  tmp = 0
+  for i in range(len(S)):
+    if S[i] == '(':
+      tmp += 1
+    elif S[i] == ')':
+      tmp -= 1
+    if tmp < 0:
+      tmp = -100
+  if tmp == 0:
+    print('YES')
+  else:
+    print('NO')
