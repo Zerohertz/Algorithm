98b5b3e69ef3db4c729cea34223d34fdfc829e15
Zerohertz <ohg3417@gmail.com>
2022-09-18 16:50:48 +0900

    Add BOJ 10816

---
+++ BOJ/Silver/10816/main.py
@@ -0,0 +1,17 @@
+M = int(input())
+ncard = list(map(int, input().split()))
+ndict = dict()
+for i in ncard:
+  if i in ndict:
+    ndict[i] += 1
+  else:
+    ndict[i] = 1
+
+N = int(input())
+scard = list(map(int, input().split()))
+
+for i in scard:
+  if i in ndict:
+    print(ndict[i], end = ' ')
+  else:
+    print(0, end = ' ')
