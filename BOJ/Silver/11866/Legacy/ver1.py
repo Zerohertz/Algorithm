bb7077dd23194dca11172b1927d194b22035141b
Zerohertz <ohg3417@gmail.com>
2022-09-18 17:25:09 +0900

    Add BOJ 11866

---
+++ BOJ/Silver/11866/Legacy/ver1.py
@@ -0,0 +1,20 @@
+N, K = map(int, input().split())
+
+l = [1] + [0 for i in range(N)]
+
+print('<', end = '')
+tmp = 0
+for i in range(N):
+  localtmp = 0
+  while localtmp < K:
+    if l[tmp % N + 1] == 0:
+      localtmp += 1
+    tmp += 1
+  if tmp > N:
+    tmp = tmp % N
+  l[tmp] = 1
+  if i < N - 1:
+    print(str(tmp) + ',', end = ' ')
+  else:
+    print(str(tmp), end = '')
+print('>')
