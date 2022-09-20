2e35cb7fbc6efc74cc1b2b6d4cc85f3cdd02363e
zerohertz <ohg3417@gmail.com>
2022-09-20 20:21:44 +0900

    Add BOJ 1931

---
+++ BOJ/Silver/1931/main.py
@@ -0,0 +1,17 @@
+N = int(input())
+l = []
+
+for i in range(N):
+  l.append(list(map(int, input().split())))
+
+l.sort(key = lambda x: (x[1], x[0]))
+
+last = 0
+cnt = 0
+
+for i in range(len(l)):
+  if l[i][0] >= last:
+    last = l[i][1]
+    cnt += 1
+
+print(cnt)
