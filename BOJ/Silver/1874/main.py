d0e9858601906f307a0d4062e18c6f3881f7de47
zerohertz <ohg3417@gmail.com>
2022-09-22 09:46:11 +0900

    Add BOJ 1874

---
+++ BOJ/Silver/1874/main.py
@@ -0,0 +1,25 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+st = []
+res = []
+cnt = 1
+tmp = True
+for i in range(n):
+  num = int(read())
+  while cnt <= num:
+    st.append(cnt)
+    res.append('+')
+    cnt += 1
+  if st[-1] == num:
+    st.pop()
+    res.append('-')
+  else:
+    tmp = False
+
+if tmp == False:
+  print('NO')
+else:
+  for i in res:
+    print(i)
