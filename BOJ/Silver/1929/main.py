e2a73d17dad8a11b7ca58f3d278c7f28ebfc30bb
zerohertz <ohg3417@gmail.com>
2022-09-22 10:46:51 +0900

    Add BOJ 1929

---
+++ BOJ/Silver/1929/main.py
@@ -0,0 +1,20 @@
+def returnPN(M, N):
+  l = []
+  for i in range(M, N + 1):
+    if i == 1:
+      continue
+    for j in range(2, int(i ** 0.5) + 1):
+      if i % j == 0:
+        break
+    else:
+      l.append(i)
+  return l
+
+import sys
+read = sys.stdin.readline
+
+M, N = map(int, read().split())
+
+l = returnPN(M, N)
+for i in l:
+  print(i)
