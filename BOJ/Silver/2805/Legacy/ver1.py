725a952d9c4852a46bf14db4dd73143d1f44312f
zerohertz <ohg3417@gmail.com>
2022-09-22 11:40:03 +0900

    Add BOJ 2805

---
+++ BOJ/Silver/2805/Legacy/ver1.py
@@ -0,0 +1,15 @@
+import sys
+read = sys.stdin.readline
+
+N, M = map(int, read().split())
+l = list(map(int, read().split()))
+
+tmp = max(l) - 1
+res = 0
+while res < M:
+  res = 0
+  for i in range(N):
+    if l[i] > tmp:
+      res += l[i] - tmp
+  tmp -= 1
+print(tmp + 1)
---
+++ BOJ/Silver/2805/main.py
@@ -0,0 +1,18 @@
+import sys
+read = sys.stdin.readline
+
+N, M = map(int, read().split())
+l = list(map(int, read().split()))
+
+bot, top = 1, max(l)
+while bot <= top:
+  res = 0
+  mid = (bot + top) // 2
+  for i in range(N):
+    if l[i] > mid:
+      res += l[i] - mid
+  if res >= M:
+    bot = mid + 1
+  else:
+    top = mid - 1
+print(top)
