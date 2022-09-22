8afb8efaa6221ee853c79eddd2ec00c4c3effe46
zerohertz <ohg3417@gmail.com>
2022-09-22 14:36:14 +0900

    Add BOJ 11651

---
+++ BOJ/Silver/11651/main.py
@@ -0,0 +1,11 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+l = []
+for i in range(N):
+  l.append(list(map(int, read().split())))
+
+l.sort(key = lambda x: (x[1], x[0]))
+for i in range(N):
+  print(l[i][0], l[i][1])
