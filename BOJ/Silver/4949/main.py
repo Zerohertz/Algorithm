742fa219214238ec8bf61f0d5a685143837ea187
zerohertz <ohg3417@gmail.com>
2022-09-23 13:55:47 +0900

    Add BOJ 4949

---
+++ BOJ/Silver/4949/main.py
@@ -0,0 +1,25 @@
+while True:
+  str = input()
+  if str == ".":
+    break
+  s = []
+  tmp = True
+  for i in str:
+    if i == '(' or i == '[':
+      s.append(i)
+    elif i == ')':
+      if not s or s[-1] == '[':
+        tmp = False
+        break
+      elif s[-1] == '(':
+        s.pop()
+    elif i == ']':
+      if not s or s[-1] == '(':
+        tmp = False
+        break
+      elif s[-1] == '[':
+        s.pop()
+  if tmp == True and not s:
+    print('yes')
+  else:
+    print('no')
