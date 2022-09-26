d2954c4a8f8700e2e898c03294e33ea06df25b2b
zerohertz <ohg3417@gmail.com>
2022-09-26 18:37:47 +0900

    Add BOJ 17214 (Incompleted)

---
+++ BOJ/Gold/17214 (Incompleted)/Legacy/ver1.py
@@ -0,0 +1,15 @@
+st = input().split('+')
+res = ''
+
+for i in range(len(st)):
+  if st[i][-1] == 'x':
+    tmp = int(st[i][:-1]) / 2
+    res = str(int(tmp)) + 'xx'
+  else:
+    if len(res):
+      res += '+' + str(int(st[i][:])) + 'x'
+    else:
+      res += str(int(st[i][:])) + 'x'
+res += '+W'
+
+print(res)
