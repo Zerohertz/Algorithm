92c08e9d932398858e46e0ea43c13a1b3ae54457
zerohertz <ohg3417@gmail.com>
2022-09-26 17:24:48 +0900

    Add BOJ 14786

---
+++ BOJ/Gold/14786/main.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+def f(A, B, C, x):
+  return C - A * x - B * math.sin(x)
+
+def fp(A, B, x):
+  return - A - B * math.cos(x)
+
+def newrap(A, B, C, x):
+  return x - f(A, B, C, x) / fp(A, B, x)
+
+read = sys.stdin.readline
+A, B, C = map(int, read().split())
+
+tmp = C / A
+
+for i in range(3000):
+  tmp = newrap(A, B, C, tmp)
+
+print(tmp)
