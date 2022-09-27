0677bee42a9c59a3090f61a11ae90ee1b4978ad8
zerohertz <ohg3417@gmail.com>
2022-09-27 10:27:07 +0900

    Add BOJ 17214

---
+++ BOJ/Gold/17214/main.py
@@ -0,0 +1,49 @@
+st = input()
+tmp = ''
+status = False
+res = ''
+
+try:
+  iszero = int(st)
+except:
+  iszero = 1
+
+for i in range(len(st)):
+  if 'x' in st:
+    if st[i] == 'x':
+      if int(tmp) // 2 == 1:
+        res = 'xx'
+      elif int(tmp) // 2 == -1:
+        res = '-xx'
+      else:
+        res = str(int(tmp) // 2) + 'xx'
+      tmp = ''
+      status = True
+    else:
+      if status:
+        tmp += st[i]
+        if i + 1 == len(st):
+          if int(tmp) == 1:
+            res += '+x'
+          elif int(tmp) == -1:
+            res += '-x'
+          else:
+            res += tmp + 'x'
+      else:
+        tmp += st[i]
+  else:
+    tmp += st[i]
+    if i + 1 == len(st):
+      if int(tmp) == 1:
+        res = 'x'
+      elif int(tmp) == -1:
+        res = '-x'
+      else:
+        res += tmp + 'x'
+
+if iszero == 0:
+  res = 'W'
+else:
+  res += '+W'
+
+print(res)
