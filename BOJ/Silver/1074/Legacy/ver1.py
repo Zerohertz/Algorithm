99f20b4d67060bd37718ced7d5684b63f56909a1
zerohertz <ohg3417@gmail.com>
2022-09-28 13:51:39 +0900

    Add BOJ 1074

---
+++ BOJ/Silver/1074/Legacy/ver1.py
@@ -0,0 +1,25 @@
+import sys
+read = sys.stdin.readline
+
+tmp = [0]
+
+def zzz(x, y, n):
+    if n != 2:
+        for tmpx in range(x, x + n):
+            for tmpy in range(y, y + n):
+                zzz(x, y, n // 2)
+                zzz(x, y + n // 2, n // 2)
+                zzz(x + n // 2, y, n // 2)
+                zzz(x + n // 2, y + n // 2, n // 2)
+                return
+    else:
+        for tmpx in range(x, x + n):
+            for tmpy in range(y, y + n):
+                l[tmpx][tmpy] = tmp[0]
+                tmp[0] += 1
+
+N, c, r = map(int, read().split())
+l = [[-1 for _ in range(2 ** N)] for _ in range(2 ** N)]
+
+zzz(0, 0, 2 ** N)
+print(l[c][r])
---
+++ BOJ/Silver/1074/main.py
@@ -0,0 +1,11 @@
+import sys
+read = sys.stdin.readline
+
+def zzz(N, r, c):
+  if N == 0:
+    return 0
+  else:
+    return 2 * (r % 2) + (c % 2) + 4 * zzz(N - 1, r // 2, c // 2)
+
+N, r, c = map(int, read().split())
+print(zzz(N, r, c))
