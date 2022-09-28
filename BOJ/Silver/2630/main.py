a829907eedba13710bd245dd1a7cd5166fd8659d
zerohertz <ohg3417@gmail.com>
2022-09-28 12:04:13 +0900

    Add BOJ 2630

---
+++ BOJ/Silver/2630/main.py
@@ -0,0 +1,28 @@
+import sys
+read = sys.stdin.readline
+
+def splitPaper(x, y, n):
+  c = l[x][y]
+  for tmpx in range(x, x + n):
+    for tmpy in range(y, y + n):
+      if c != l[tmpx][tmpy]:
+        splitPaper(x, y, n // 2)
+        splitPaper(x + n // 2, y, n // 2)
+        splitPaper(x, y + n // 2, n // 2)
+        splitPaper(x + n // 2, y + n // 2, n // 2)
+        return
+  if c == 0:
+    res.append(0)
+  else:
+    res.append(1)
+
+N = int(read())
+l = [[0 for _ in range(N)] for _ in range(N)]
+
+for i in range(N):
+  l[i] = list(map(int, read().split()))
+
+res = []
+splitPaper(0, 0, N)
+print(res.count(0))
+print(res.count(1))
