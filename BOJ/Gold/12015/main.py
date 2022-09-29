025e82f32e3d9a1b22a3d3e3fc3cbbdb92460957
zerohertz <ohg3417@gmail.com>
2022-09-29 13:30:09 +0900

    Add BOJ 12015

---
+++ BOJ/Gold/12015/main.py
@@ -0,0 +1,22 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+A = list(map(int, read().split()))
+ser = [0]
+
+for i in A:
+  if ser[-1] < i:
+    ser.append(i)
+  else:
+    left = 0
+    right = len(ser)
+    while left < right:
+      mid = (left + right) // 2
+      if ser[mid] < i:
+        left = mid + 1
+      else:
+        right = mid
+    ser[right] = i
+
+print(len(ser) - 1)
