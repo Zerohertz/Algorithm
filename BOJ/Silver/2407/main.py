db2bf8be56f2acd984c7f051e6e4007fd1251571
zerohertz <ohg3417@gmail.com>
2022-09-29 13:50:05 +0900

    Add BOJ 2407

---
+++ BOJ/Silver/2407/main.py
@@ -0,0 +1,14 @@
+import sys
+read = sys.stdin.readline
+
+def factorial(m, n):
+    res = 1
+    for i in range(m, n + 1):
+        res *= i
+    return res
+
+n, m = map(int, read().split())
+if n // 2 >= m:
+    print(factorial(n - m + 1, n) // factorial(1, m))
+else:
+    print(factorial(m + 1, n) // factorial(1, n - m))
