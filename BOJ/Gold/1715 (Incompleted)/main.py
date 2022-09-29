e7b7ce27454ce1eab53c92f97fb78ec24657e508
Zerohertz <ohg3417@gmail.com>
2022-09-29 18:27:19 +0900

    Add BOJ 1715 (Incompleted)

---
+++ BOJ/Gold/1715 (Incompleted)/main.py
@@ -0,0 +1,19 @@
+import sys
+import heapq
+read = sys.stdin.readline
+
+N = int(read())
+l = []
+
+for _ in range(N):
+  heapq.heappush(l, int(read()))
+
+res = 0
+
+for i in range(N):
+  if i > 1:
+    res += res + heapq.heappop(l)
+  else:
+    res += heapq.heappop(l)
+
+print(res)
