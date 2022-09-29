f4f4dcd69332af1980d92e3687e2bf32f32a5e39
zerohertz <ohg3417@gmail.com>
2022-09-29 13:37:24 +0900

    Add BOJ 11053

---
+++ BOJ/Silver/11053/main.py
@@ -0,0 +1,13 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+A = list(map(int, read().split()))
+res = [1 for _ in range(N)]
+
+for i in range(N):
+  for j in range(i):
+    if A[i] > A[j]:
+      res[i] = max(res[i], res[j] + 1)
+
+print(max(res))
