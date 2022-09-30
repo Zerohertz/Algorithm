6abfb9ae026439be92da74558ea546ec782bcfa5
Zerohertz <ohg3417@gmail.com>
2022-09-30 18:43:51 +0900

    Add BOJ 1543

---
+++ BOJ/Silver/1543/main.py
@@ -0,0 +1,16 @@
+import sys
+read = sys.stdin.readline
+
+s1 = read().rstrip()
+s2 = read().rstrip()
+cnt = 0
+tmp = 0
+
+while tmp <= len(s1) - len(s2):
+  if s1[tmp:tmp+len(s2)] == s2:
+    cnt += 1
+    tmp += len(s2)
+  else:
+    tmp += 1
+
+print(cnt)
