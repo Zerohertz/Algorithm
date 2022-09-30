97ee748071f4f8170a963db95a3650d07befc42b
Zerohertz <ohg3417@gmail.com>
2022-09-30 19:02:05 +0900

    Add BOJ 1080

---
+++ BOJ/Silver/1080/main.py
@@ -0,0 +1,42 @@
+import sys
+read = sys.stdin.readline
+
+def cal(mat, x, y):
+    for i in range(3):
+        for j in range(3):
+            if mat[x + i][y + j] == '1':
+                mat[x + i][y + j] = '0'
+            else:
+                mat[x + i][y + j] = '1'
+
+
+N, M = map(int, read().split())
+A = [[] for _ in range(N)]
+B = [[] for _ in range(N)]
+
+for i in range(N):
+    A[i] = list(read().rstrip())
+
+for i in range(N):
+    B[i] = list(read().rstrip())
+
+cnt = 0
+
+for i in range(N - 2):
+    for j in range(M - 2):
+        if A[i][j] != B[i][j]:
+            cal(A, i, j)
+            cnt += 1
+
+status = True
+
+for i in range(N):
+    for j in range(M):
+        if A[i][j] != B[i][j]:
+            status = False
+            break
+
+if status:
+    print(cnt)
+else:
+    print(-1)
