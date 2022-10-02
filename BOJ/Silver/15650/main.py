c4e3f4e66db8068d3ae3e6eaed658bb986f12986
Zerohertz <ohg3417@gmail.com>
2022-10-02 15:10:08 +0900

    15650

---
+++ BOJ/Silver/15650/main.py
@@ -0,0 +1,17 @@
+import sys
+
+read = sys.stdin.readline
+N, M = map(int, read().split())
+l = []
+
+def DFS(start):
+    if len(l) == M:
+        print(' '.join(map(str, l)))
+        return
+    for i in range(start, N + 1):
+        if not i in l:
+            l.append(i)
+            DFS(i + 1)
+            l.pop()
+
+DFS(1)
