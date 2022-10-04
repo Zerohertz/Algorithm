ea3d14351df095111c8a2b72f2cbdc4f0b13be9b
zerohertz <ohg3417@gmail.com>
2022-10-04 09:46:21 +0900

    Add BOJ 11725

---
+++ BOJ/Silver/11725/main.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+read = sys.stdin.readline
+
+def BFS():
+    q = deque([1])
+    par = [0 for _ in range(N + 1)]
+    while q:
+        tmp = q.popleft()
+        for i in g[tmp]:
+            if par[i] == 0 and i != 1:
+                par[i] = tmp
+                q.append(i)
+    for i in range(2, N + 1):
+        print(par[i])
+
+
+N = int(read())
+
+g = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    n1, n2 = map(int, read().split())
+    g[n1].append(n2)
+    g[n2].append(n1)
+
+BFS()
