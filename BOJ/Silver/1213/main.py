5fa40ea9bb365ea060f7aa0c7b5243447855bd67
Zerohertz <ohg3417@gmail.com>
2022-10-08 19:37:08 +0900

    Add BOJ 1213

---
+++ BOJ/Silver/1213/main.py
@@ -0,0 +1,23 @@
+import sys
+from collections import Counter
+
+s = list(map(str, sys.stdin.readline().rstrip()))
+s.sort()
+c = Counter(s)
+cnt = 0
+mid = ''
+res = ''
+
+for i in c:
+  if c[i] % 2 != 0:
+    cnt += 1
+    mid += i
+    s.remove(i)
+  if cnt > 1:
+    print("I'm Sorry Hansoo")
+    exit(0)
+
+for i in range(0, len(s), 2):
+  res += s[i]
+
+print(res + mid + res[::-1])
