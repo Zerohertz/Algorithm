07c835f01010ba48b3edaf2846aa5e69dab130ed
zerohertz <ohg3417@gmail.com>
2022-10-12 11:36:53 +0900

    Add BOJ 18917

---
+++ BOJ/Silver/18917/main.py
@@ -0,0 +1,19 @@
+import sys
+read = sys.stdin.readline
+
+M = int(read())
+tmp = 0
+xor = 0
+
+for _ in range(M):
+  l = list(map(int, read().split()))
+  if l[0] == 1:
+    tmp += l[1]
+    xor ^= l[1]
+  elif l[0] == 2:
+    tmp -= l[1]
+    xor ^= l[1]
+  elif l[0] == 3:
+    print(tmp)
+  elif l[0] == 4:
+    print(xor)
