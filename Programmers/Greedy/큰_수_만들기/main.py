7bbc1446b99562ebae2c43a66e102bd2ea3f696a
zerohertz <ohg3417@gmail.com>
2022-10-13 15:44:22 +0900

    Add Programmer 큰_수_만들기

---
+++ Programmers/Greedy/큰_수_만들기/main.py
@@ -0,0 +1,9 @@
+def solution(number, k):
+    answer = []
+    for n in number:
+        while answer and k > 0 and answer[-1] < n:
+            answer.pop()
+            k -= 1
+        answer.append(n)
+    answer = ''.join(answer[:len(number)-k])
+    return answer
