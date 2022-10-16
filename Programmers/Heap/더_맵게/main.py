502cd95e17ae0af488236d56dc0ebf5fdf5031ed
zerohertz <ohg3417@gmail.com>
2022-10-16 22:04:26 +0900

    Add Programmers 더_맵게

---
+++ Programmers/Heap/더_맵게/main.py
@@ -0,0 +1,16 @@
+import heapq
+
+def solution(scoville, K):
+    answer, res = 0, 0
+    heapq.heapify(scoville)
+    try:
+        while True:
+            legacy = heapq.heappop(scoville)
+            if legacy >= K:
+                break
+            res = legacy + heapq.heappop(scoville) * 2
+            heapq.heappush(scoville, res)
+            answer += 1
+    except:
+        answer = -1
+    return answer
