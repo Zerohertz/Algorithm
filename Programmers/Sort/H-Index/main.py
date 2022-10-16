686ccbb36364e01d5aa570fb1679c89340cf89d5
zerohertz <ohg3417@gmail.com>
2022-10-16 22:36:40 +0900

    Add Programmers H-Index

---
+++ Programmers/Sort/H-Index/main.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    answer = 0
+    citations.sort(reverse = True)
+    for i, citation in enumerate(citations):
+        if i >= citation:
+            return i
+    return len(citations)
