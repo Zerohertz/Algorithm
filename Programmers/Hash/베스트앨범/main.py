b824d69677076552cd88468d5ec4191cd78ad50a
zerohertz <ohg3417@gmail.com>
2022-10-17 17:12:49 +0900

    Add Programmers 베스트앨범

---
+++ Programmers/Hash/베스트앨범/main.py
@@ -0,0 +1,28 @@
+import heapq
+
+def solution(genres, plays):
+    answer = []
+    d = {}
+    ds = {}
+    idx = 0
+    for genre, play in zip(genres, plays):
+        try:
+            heapq.heappush(d[genre], (-play, idx))
+            ds[genre] += play
+        except:
+            d[genre] = [(-play, idx)]
+            ds[genre] = play
+        idx += 1
+    l = [[] for _ in range(len(d))]
+    for i, key in zip(range(len(ds)), ds.keys()):
+        l[i].append(ds[key])
+        l[i].append(key)
+    l.sort(key = lambda x: x[0], reverse = True)
+    for i in l:
+        for _ in range(2):
+            try:
+                tmp = heapq.heappop(d[i[1]])
+                answer.append(tmp[1])
+            except:
+                continue
+    return answer
