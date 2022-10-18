97a555fef18995afbc50f4211aa2c9e9e90eb4e1
zerohertz <ohg3417@gmail.com>
2022-10-18 15:33:47 +0900

    Add Programmers 네트워크

---
+++ Programmers/DFS_BFS/네트워크/main.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def BFS(computers, n, visit):
+    q = deque()
+    cnt = 0
+    for i in range(n):
+        if not visit[i]:
+            visit[i] = True
+            q.append(i)
+            cnt += 1
+        while q:
+            tmp = q.popleft()
+            for j in range(n):
+                if computers[tmp][j] == 1 and not visit[j]:
+                    visit[j] = True
+                    q.append(j)
+    return cnt
+
+def solution(n, computers):
+    visit = [False for _ in range(n)]
+    answer = BFS(computers, n, visit)
+    return answer
