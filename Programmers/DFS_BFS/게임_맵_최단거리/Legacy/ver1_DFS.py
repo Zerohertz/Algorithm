f182b454e5ffbbda9f8288b1a9e577d78cf9e6e4
zerohertz <ohg3417@gmail.com>
2022-10-18 14:38:17 +0900

    Add Programmers 게임_맵_최단거리

---
+++ Programmers/DFS_BFS/게임_맵_최단거리/Legacy/ver1_DFS.py
@@ -0,0 +1,23 @@
+def DFS(maps, pos, cnt):
+    v1 = [1, 0, -1, 0]
+    v2 = [0, 1, 0, -1]
+    N = len(maps)
+    M = len(maps[0])
+    if pos[0] == N - 1 and pos[1] == M - 1:
+        return maps
+    else:
+        for i, j in zip(v1, v2):
+            if pos[0] + i > -1 and pos[1] + j > -1 and pos[0] + i < N and pos[1] + j < M:
+                if maps[pos[0] + i][pos[1] + j] == 1:
+                    cnt += 1
+                    maps[pos[0] + i][pos[1] + j] = cnt
+                    DFS(maps, [pos[0] + i, pos[1] + j], cnt)
+    return maps
+
+def solution(maps):
+    maps[0][0] = 0
+    answer = DFS(maps, [0, 0], 1)
+    if answer[-1][-1] != 1:
+        return answer[-1][-1]
+    else:
+        return -1
---
+++ Programmers/DFS_BFS/게임_맵_최단거리/main.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def BFS(maps):
+    v1 = [1, 0, -1, 0]
+    v2 = [0, 1, 0, -1]
+    q = deque([[0, 0]])
+    N = len(maps)
+    M = len(maps[0])
+    maps[0][0] = 101
+    while q:
+        tmp = q.popleft()
+        for i, j in zip(v1, v2):
+            if tmp[0] + i > -1 and tmp[1] + j > -1 and tmp[0] + i < N and tmp[1] + j < M:
+                if maps[tmp[0] + i][tmp[1] + j] == 1:
+                    q.append([tmp[0] + i, tmp[1] + j])
+                    maps[tmp[0] + i][tmp[1] + j] = maps[tmp[0]][tmp[1]] + 1
+    return maps
+
+def solution(maps):
+    answer = BFS(maps)
+    if answer[-1][-1] != 1:
+        return answer[-1][-1] - 100
+    else:
+        return -1
