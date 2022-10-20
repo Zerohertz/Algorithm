183be5737194d174370e316ebf7e9b3a4c7883c2
Zerohertz <ohg3417@gmail.com>
2022-10-20 11:27:10 +0900

    Add Programmers 소수_찾기

---
+++ Programmers/Brute_Force/소수_찾기/main.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+
+def cPN(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    answer = []
+    tmp = []
+    numbers = list(numbers)
+    for i in range(1, len(numbers) + 1):
+        tmp += permutations(numbers, i)
+    num = [int(''.join(t)) for t in tmp]
+    for i in num:
+        if cPN(i):
+            answer.append(i)
+    return len(set(answer))
