4f1f1d510cd55e5362b8925be922fd492adb6e8d
Zerohertz <ohg3417@gmail.com>
2022-10-20 23:00:12 +0900

    Add Programmers 다리를_지나는_트럭

---
+++ Programmers/Stack_and_Queue/다리를_지나는_트럭/main.py
@@ -0,0 +1,16 @@
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    time = 0
+    truck_weights = deque(truck_weights)
+    tmp = deque([0 for _ in range(bridge_length)])
+    tmpw = 0
+    while tmp:
+        time += 1
+        tmpw -= tmp.popleft()
+        if truck_weights:
+            if tmpw + truck_weights[0] <= weight:
+                tmpw += truck_weights[0]
+                tmp.append(truck_weights.popleft())
+            else:
+                tmp.append(0)
+    return time
