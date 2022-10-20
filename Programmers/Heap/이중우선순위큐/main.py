46e794ace9bb1eb85bb94031ea397fb090d9c664
Zerohertz <ohg3417@gmail.com>
2022-10-20 23:45:30 +0900

    Add Programmers 이중우선순위큐

---
+++ Programmers/Heap/이중우선순위큐/main.py
@@ -0,0 +1,24 @@
+import heapq
+
+def solution(operations):
+    mheap = []
+    Mheap = []
+    for oper in operations:
+        tmpoper = oper.split()
+        if tmpoper[0] == 'I':
+            heapq.heappush(mheap, int(tmpoper[1]))
+            heapq.heappush(Mheap, ((-int(tmpoper[1])), int(tmpoper[1])))
+        elif tmpoper[0] == 'D':
+            if not mheap:
+                pass
+            elif tmpoper[1] == '1':
+                tmp = heapq.heappop(Mheap)
+                mheap.remove(tmp[1])
+            elif tmpoper[1] == '-1':
+                tmp = heapq.heappop(mheap)
+                Mheap.remove((-tmp, tmp))
+    if mheap:
+        answer = [heapq.heappop(Mheap)[1], heapq.heappop(mheap)]
+    else:
+        answer = [0, 0]
+    return answer
