098e64f3dc52e7e22f9089be8655f204f1fc7e88
zerohertz <ohg3417@gmail.com>
2022-10-24 12:05:25 +0900

    Add BOJ 1865

---
+++ BOJ/Gold/1865/main.py
@@ -0,0 +1,34 @@
+import sys
+read = sys.stdin.readline
+INF = int(1e9)
+
+def bf(start):
+  distance = [INF for _ in range(N + 1)]
+  distance[start] = 0
+  for i in range(N):
+    for road in roads:
+      tmp = road[0]
+      next = road[1]
+      time = road[2]
+      if distance[next] > time + distance[tmp]:
+        distance[next] = time + distance[tmp]
+        if i == N - 1:
+          return True
+  return False
+
+TC = int(read())
+
+for _ in range(TC):
+  N, M, W = map(int, read().split())
+  roads = []
+  for _ in range(M):
+    S, E, T = map(int, read().split())
+    roads.append((S, E, T))
+    roads.append((E, S, T))
+  for _ in range(W):
+    S, E, T = map(int, read().split())
+    roads.append((S, E, -T))
+  if bf(1):
+    print("YES")
+  else:
+    print("NO")
