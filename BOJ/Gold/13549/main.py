4e0371fada55d55b99f4709efcdfb58164e755f2
Zerohertz <ohg3417@gmail.com>
2022-10-25 12:17:41 +0900

    Add BOJ 13549

---
+++ BOJ/Gold/13549/main.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+N, K = map(int, sys.stdin.readline().split())
+
+q = deque([N])
+lt = [-1 for _ in range(100_001)]
+lt[N] = 0
+while q:
+  tmp = q.popleft()
+  if tmp == K:
+    break
+  if 2 * tmp <= 100_000 and lt[2 * tmp] == -1:
+    lt[2 * tmp] = lt[tmp]
+    q.appendleft(2 * tmp)
+  if tmp - 1 >= 0 and tmp - 1 <= 100_000 and lt[tmp - 1] == -1:
+    lt[tmp - 1] = lt[tmp] + 1
+    q.append(tmp - 1)
+  if tmp + 1 >= 0 and tmp + 1 <= 100_000 and lt[tmp + 1] == -1:
+    lt[tmp + 1] = lt[tmp] + 1
+    q.append(tmp + 1)
+
+print(lt[K])
