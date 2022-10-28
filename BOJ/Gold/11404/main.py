dc85ac404f2d0015b76353f9729283e0d5754c12
Zerohertz <ohg3417@gmail.com>
2022-10-28 14:19:23 +0900

    Edit BOJ 11404

--- BOJ/Gold/11404/main.py
+++ BOJ/Gold/11404/main.py
@@ -4,22 +4,23 @@ read = sys.stdin.readline
 n = int(read())
 m = int(read())
 
-res = [[100_001 for _ in range(n)] for _ in range(n)]
+INF = 100_000_000
+res = [[INF for _ in range(n)] for _ in range(n)]
 
-for i in range(m):
+for _ in range(m):
     a, b, c = map(int, read().split())
-    if res[a - 1][b - 1] > c:
-        res[a - 1][b - 1] = c
+    res[a - 1][b - 1] = min(res[a - 1][b - 1], c)
 
 for i in range(n):
     for j in range(n):
-        if i == j:
-            res[i][j] = 0
         for k in range(n):
-            if i != j:
+            if j != k:
                 res[j][k] = min(res[j][k], res[j][i] + res[i][k])
 
 for i in res:
     for j in i:
-        print(j, end=' ')
+        if j == INF:
+            print(0, end=' ')
+        else:
+            print(j, end=' ')
     print()
