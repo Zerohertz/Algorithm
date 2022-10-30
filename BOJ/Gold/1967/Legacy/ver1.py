c19554a27ff0bc5bfd2b2445705004c2d4f19b9c
Zerohertz <ohg3417@gmail.com>
2022-10-30 22:53:32 +0900

    Add BOJ 1967

---
+++ BOJ/Gold/1967/Legacy/ver1.py
@@ -0,0 +1,42 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+l = [[] for _ in range(n + 1)]
+par = []
+
+for _ in range(n - 1):
+  a, b, c = map(int, read().split())
+  par.append(a)
+  l[a].append((b, c))
+
+par = set(par)
+
+def DFS(p, tmp, lr):
+  length = len(l[p])
+  if length >= 2:
+    if tmp == 0:
+      for i in range(length):
+        DFS(l[p][i][0], tmp + l[p][i][1], i)
+    else:
+      for i in range(length):
+        DFS(l[p][i][0], tmp + l[p][i][1], lr)
+  elif len(l[p]) == 1:
+    DFS(l[p][0][0], tmp + l[p][0][1], lr)
+  elif len(l[p]) == 0:
+    res[lr].append(tmp)
+
+dia = []
+
+for i in par:
+  leng = len(l[i])
+  if leng > 1:
+    res = [[] for _ in range(leng)]
+    DFS(i, 0, 0)
+    resmax = [0 for _ in range(leng)]
+    for i in range(leng):
+      resmax[i] = max(res[i])
+    resmax.sort(reverse = True)
+    dia.append(resmax[0] + resmax[1])
+
+print(max(dia))
---
+++ BOJ/Gold/1967/main.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(10**9)
+read = sys.stdin.readline
+
+n = int(read())
+l = [[] for _ in range(n + 1)]
+
+for _ in range(n - 1):
+  a, b, c = map(int, read().split())
+  l[a].append((b, c))
+  l[b].append((a, c))
+
+def DFS(p, tmp):
+  for b, c in l[p]:
+    if dia[b] == -1:
+      dia[b] = tmp + c
+      DFS(b, tmp + c)
+
+dia = [-1 for _ in range(n + 1)]
+dia[1] = 0
+DFS(1, 0)
+
+init = dia.index(max(dia))
+dia = [-1 for _ in range(n + 1)]
+dia[init] = 0
+DFS(init, 0)
+
+print(max(dia))
