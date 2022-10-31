6f88ce75b0a475e0a56b4a1d212824be32f58d7e
Zerohertz <ohg3417@gmail.com>
2022-11-01 01:11:52 +0900

    Add BOJ 1062 (Incompleted)

---
+++ BOJ/Gold/1062 (Incompleted)/Legacy/ver1.py
@@ -0,0 +1,35 @@
+import sys
+from itertools import combinations
+
+read = sys.stdin.readline
+
+N, K = map(int, read().split())
+
+s = set()
+wordset = []
+antic = set('antic')
+tupleantic = tuple(antic)
+
+for i in range(N):
+  word = read().rstrip()
+  tmpwordset = set(word) - antic
+  s |= tmpwordset
+  wordset.append(tmpwordset)
+
+res = 0
+if K >= 5:
+  for i in combinations(s, K - 5):
+    tmp = 0
+    tmpi = tupleantic + i
+    for j in wordset:
+      if j - set(tmpi) == set():
+        tmp += 1
+    res = max(res, tmp)
+elif K == 5:
+  tmp = 0
+  for j in wordset:
+    if j - set(antic) == set():
+      tmp += 1
+  res = tmp
+
+print(res)
