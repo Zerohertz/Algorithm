4538f84791fb6dfe9cd694853ef40146853456be
zerohertz <ohg3417@gmail.com>
2022-11-02 09:52:58 +0900

    Add BOJ 1057

---
+++ BOJ/Silver/1057/main.py
@@ -0,0 +1,19 @@
+import sys
+read = sys.stdin.readline
+
+N, A, B = map(int, read().split())
+
+res = 1
+
+while N > 1:
+  if N % 2 == 0:
+    N = N // 2
+  else:
+    N = N // 2 + 1
+  A = round(A / 2 + 0.01)
+  B = round(B / 2 + 0.01)
+  if A == B:
+    break
+  res += 1
+
+print(res)
