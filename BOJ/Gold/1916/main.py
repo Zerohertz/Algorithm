2410ff8e371974d91e3cbee98e27c6c0407948fc
zerohertz <ohg3417@gmail.com>
2022-11-15 16:52:05 +0900

    Add BOJ 1916

---
+++ BOJ/Gold/1916/main.py
@@ -0,0 +1,31 @@
+import sys
+import heapq
+
+read = sys.stdin.readline
+
+N = int(read())
+M = int(read())
+h = []
+g = [[] for _ in range(N)]
+
+for _ in range(M):
+    a, b, c = map(int, read().split())
+    g[a - 1].append((b - 1, c))
+
+start, end = map(int, read().split())
+
+d = [sys.maxsize for _ in range(N)]
+d[start - 1] = 0
+heapq.heappush(h, (0, start - 1))
+
+while h:
+    tmpw, tmpv = heapq.heappop(h)
+    if d[tmpv] < tmpw:
+        continue
+    for nv, nw in g[tmpv]:
+        nextw = tmpw + nw
+        if nextw < d[nv]:
+            d[nv] = nextw
+            heapq.heappush(h, (nextw, nv))
+
+print(d[end - 1])
