cd2ace8614c512f4bad476f35635bfbf2e37754d
Zerohertz <ohg3417@gmail.com>
2022-11-17 18:46:51 +0900

    Add BOJ 2003

---
+++ BOJ/Silver/2003/main.py
@@ -0,0 +1,19 @@
+import sys
+read = sys.stdin.readline
+
+N, M = map(int, read().split())
+l = list(map(int, read().split()))
+
+tmp = 0
+res = 0
+idx2 = 0
+
+for idx1 in range(N):
+  while tmp < M and idx2 < N:
+    tmp += l[idx2]
+    idx2 += 1
+  if tmp == M:
+    res += 1
+  tmp -= l[idx1]
+
+print(res)
