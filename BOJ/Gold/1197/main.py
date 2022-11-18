8e28d5b7df7228c423b7ac4a4a185be7117d352d
zerohertz <ohg3417@gmail.com>
2022-11-18 10:55:54 +0900

    Add BOJ 1197

---
+++ BOJ/Gold/1197/main.py
@@ -0,0 +1,30 @@
+import sys
+import heapq
+
+read = sys.stdin.readline
+
+V, E = map(int, read().split())
+G = [[] for _ in range(V + 1)]
+
+for _ in range(E):
+    a, b, c = map(int, read().split())
+    G[b].append([c, a])
+    G[a].append([c, b])
+
+l = [[0, 1]]
+visit = [False for _ in range(V + 1)]
+cnt = 0
+cost = 0
+
+while l:
+    if cnt == V:
+        break
+    c, a = heapq.heappop(l)
+    if not visit[a]:
+        visit[a] = True
+        cost += c
+        cnt += 1
+        for i in G[a]:
+            heapq.heappush(l, i)
+
+print(cost)
