71e736c798620218163f5791bb1209a223440c80
Zerohertz <ohg3417@gmail.com>
2022-11-19 11:08:24 +0900

    Add BOJ 1940

---
+++ BOJ/Silver/1940/main.py
@@ -0,0 +1,28 @@
+N = int(input())
+M = int(input())
+l = list(map(int, input().split()))
+l.sort()
+
+idx1 = 0
+idx2 = N - 1
+tmp = l[idx1] + l[idx2]
+cnt = 0
+
+while True:
+  if tmp < M:
+    tmp -= l[idx1]
+    idx1 += 1
+    tmp += l[idx1]
+  elif tmp > M:
+    tmp -= l[idx2]
+    idx2 -= 1
+    tmp += l[idx2]
+  else:
+    cnt += 1
+    idx1 += 1
+    idx2 -= 1
+    tmp = l[idx1] + l[idx2]
+  if idx1 >= idx2:
+    break
+
+print(cnt)
