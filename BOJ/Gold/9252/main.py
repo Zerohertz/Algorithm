f15c964b282daf94d45088a373966686b0db3215
zerohertz <ohg3417@gmail.com>
2022-11-24 12:51:09 +0900

    Add BOJ 9252

---
+++ BOJ/Gold/9252/main.py
@@ -0,0 +1,22 @@
+import sys
+
+A = [0] + list(sys.stdin.readline().rstrip())
+B = [0] + list(sys.stdin.readline().rstrip())
+
+Na = len(A)
+Nb = len(B)
+
+dp = [["" for _ in range(Nb)] for _ in range(Na)]
+
+for i in range(1, Na):
+  for j in range(1, Nb):
+    if A[i] == B[j]:
+      dp[i][j] = dp[i - 1][j - 1] + A[i]
+    else:
+      if len(dp[i - 1][j]) > len(dp[i][j - 1]):
+        dp[i][j] = dp[i - 1][j]
+      else:
+        dp[i][j] = dp[i][j - 1]
+
+print(len(dp[Na - 1][Nb - 1]))
+print(dp[Na - 1][Nb - 1])
