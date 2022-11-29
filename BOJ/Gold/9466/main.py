9229c8e4e86de0f531731f1a071f0642f9d61a3f
zerohertz <ohg3417@gmail.com>
2022-11-29 11:24:36 +0900

    Add BOJ 9466

---
+++ BOJ/Gold/9466/main.py
@@ -0,0 +1,30 @@
+import sys
+
+read = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+T = int(read())
+
+
+def DFS(pos):
+    global res
+    visit[pos] = True
+    tmp.append(pos)
+    if visit[l[pos]]:
+        if l[pos] in tmp:
+            res += tmp[tmp.index(l[pos]):]
+        return
+    else:
+        DFS(l[pos])
+
+
+for _ in range(T):
+    n = int(read())
+    l = [0] + list(map(int, read().split()))
+    visit = [False for _ in range(n + 1)]
+    res = []
+    for i in range(1, n + 1):
+        if not visit[i]:
+            tmp = []
+            DFS(i)
+    print(n - len(res))
