f80573bb0c25df46052c3d22109a530388bb5405
Zerohertz <ohg3417@gmail.com>
2022-12-01 00:28:18 +0900

    Add Codility StrSymmetryPoint

---
+++ Codility/Exercises/Algorithmic_skills/StrSymmetryPoint/README.md
@@ -0,0 +1,1 @@
+[Solution](https://app.codility.com/demo/results/trainingPM6FE3-G57/)
---
+++ Codility/Exercises/Algorithmic_skills/StrSymmetryPoint/main.py
@@ -0,0 +1,16 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(S):
+    # write your code in Python 3.8.10
+    N = len(S)
+    res = 0
+    for i in range(N // 2):
+        if S[i] == S[N - i - 1]:
+            res += 1
+        else:
+            return -1
+    if N % 2 == 0:
+        return -1
+    else:
+        return res
