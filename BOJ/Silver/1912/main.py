672484d5f30c805d79b0bb0515545481f90cf551
Zerohertz <ohg3417@gmail.com>
2022-12-01 18:20:15 +0900

    Add BOJ 1912

---
+++ BOJ/Silver/1912/main.py
@@ -0,0 +1,12 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+l = list(map(int, read().split()))
+
+dp = [l[0]]
+
+for i in range(1, n):
+  dp.append(max(l[i], dp[i - 1] + l[i]))
+
+print(max(dp))
