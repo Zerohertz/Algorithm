11c740f8c5d30f0b475465bdb7dc9de7b756d112
Zerohertz <ohg3417@gmail.com>
2022-12-04 21:50:44 +0900

    Add BOJ 1092

---
+++ BOJ/Gold/1092/main.py
@@ -0,0 +1,23 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+cranes = list(map(int, read().split()))
+M = int(read())
+boxs = list(map(int, read().split()))
+
+cranes.sort(reverse = True)
+boxs.sort(reverse = True)
+
+if boxs[0] > cranes[0]:
+  print(-1)
+else:
+  time = 0
+  while boxs:
+    for crane in cranes:
+      for box in boxs:
+        if crane >= box:
+          boxs.remove(box)
+          break
+    time += 1
+  print(time)
