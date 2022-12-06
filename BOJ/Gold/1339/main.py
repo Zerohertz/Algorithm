35cf610823a13ef13a1e55a64917c7fb2b4a0fad
zerohertz <ohg3417@gmail.com>
2022-12-06 11:28:58 +0900

    Add BOJ 1339

---
+++ BOJ/Gold/1339/main.py
@@ -0,0 +1,20 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+l = [0 for _ in range(26)]
+
+for i in range(N):
+  tmp = read().rstrip()
+  for j, k in enumerate(tmp[::-1]):
+    l[ord(k) - ord('A')] += 10 ** j
+
+l.sort(reverse = True)
+
+res = 0
+tmp = 9
+for i in l[:9]:
+  res += i * tmp
+  tmp -= 1
+
+print(res)
