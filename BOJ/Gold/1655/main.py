6c35cf16f072579d8524da2d4d3899ed27370d24
Zerohertz <ohg3417@gmail.com>
2022-12-07 22:22:19 +0900

    Add BOJ 1655

---
+++ BOJ/Gold/1655/main.py
@@ -0,0 +1,22 @@
+import sys
+import heapq
+
+read = sys.stdin.readline
+
+N = int(read())
+
+l = []
+r = []
+
+for i in range(N):
+  tmp = int(read())
+  if len(l) == len(r):
+    heapq.heappush(l, (-tmp, tmp))
+  else:
+    heapq.heappush(r, (tmp, tmp))
+  if r and l[0][1] > r[0][1]:
+    min = heapq.heappop(r)[1]
+    max = heapq.heappop(l)[1]
+    heapq.heappush(l, (-min, min))
+    heapq.heappush(r, (max, max))
+  print(l[0][1])
