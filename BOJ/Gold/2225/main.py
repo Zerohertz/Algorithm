f22ec1de515f6eecf00dbf6bbda81ff576a8283e
Zerohertz <ohg3417@gmail.com>
2022-12-11 19:48:08 +0900

    Add BOJ 2225

---
+++ BOJ/Gold/2225/main.py
@@ -0,0 +1,16 @@
+import sys
+read = sys.stdin.readline
+
+N, K = map(int, read().split())
+dp = [[0 for _ in range(201)] for _ in range(201)]
+
+for i in range(201):
+  dp[1][i] = 1
+  dp[2][i] = i + 1
+
+for i in range(2, 201):
+  dp[i][1] = i
+  for j in range(2, 201):
+    dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1_000_000_000
+
+print(dp[K][N])
