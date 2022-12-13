bd08ea31a6a08f3ea5c29df4de430ea222bfd079
Zerohertz <ohg3417@gmail.com>
2022-12-13 22:59:48 +0900

    Add BOJ 2133

---
+++ BOJ/Gold/2133/main.py
@@ -0,0 +1,14 @@
+import sys
+read = sys.stdin.readline
+
+dp = [0 for _ in range(31)]
+dp[2] = 3
+dp[4] = 11
+
+for i in range(6, 31, 2):
+    dp[i] = 3 * dp[i - 2]
+    for j in range(4, i, 2):
+        dp[i] += 2 * dp[i - j]
+    dp[i] += 2
+
+print(dp[int(read())])
