a3e47de5a4da95cbd5eed8682a9b32aad1bdb289
zerohertz <ohg3417@gmail.com>
2022-12-28 15:11:38 +0900

    Add BOJ 1013

---
+++ BOJ/Gold/1013/main.py
@@ -0,0 +1,13 @@
+import re
+import sys
+read = sys.stdin.readline
+
+T = int(read())
+for _ in range(T):
+  S = read().strip()
+  p = re.compile('(100+1+|01)+')
+  res = p.fullmatch(S)
+  if res:
+    print("YES")
+  else:
+    print("NO")
