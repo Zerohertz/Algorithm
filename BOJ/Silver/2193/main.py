9890f20f05e05ffea0b3f4402df8471f122e5502
Zerohertz <ohg3417@gmail.com>
2023-02-08 00:19:57 +0900

    Add BOJ 2193

---
+++ BOJ/Silver/2193/main.py
@@ -0,0 +1,13 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+
+l = [0 for _ in range(91)]
+l[1] = 1
+l[2] = 1
+l[3] = 2
+for i in range(3, N + 1):
+    l[i] = l[i - 1] + l[i - 2]
+
+print(l[N])
