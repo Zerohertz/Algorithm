a27cb71c1ad80db0d7ebd40fd7fb3f25781ad747
Zerohertz <ohg3417@gmail.com>
2023-04-11 21:16:58 +0900

    :fire: Add: BOJ 18258

---
+++ BOJ/Silver/18258/main.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+read = sys.stdin.readline
+
+def comm(s):
+    if 'push' == s[0]:
+        q.append(int(s[1]))
+    elif 'pop' == s[0]:
+        try:
+            print(q.popleft())
+        except:
+            print(-1)
+    elif 'size' == s[0]:
+        print(len(q))
+    elif 'empty' == s[0]:
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif 'front' == s[0]:
+        try:
+            print(q[0])
+        except:
+            print(-1)
+    elif 'back' == s[0]:
+        try:
+            print(q[-1])
+        except:
+            print(-1)
+
+
+if __name__ == "__main__":
+    q = deque([])
+    N = int(read())
+    for _ in range(N):
+        s = read().strip().split()
+        comm(s)
