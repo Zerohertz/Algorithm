187af9eabe5a9b33b275272641cf6ed71ebaf86d
Zerohertz <ohg3417@gmail.com>
2023-05-12 16:55:27 +0900

    :computer: Add: BOJ 1308

---
+++ BOJ/Silver/1308/main.py
@@ -0,0 +1,19 @@
+import sys
+import datetime
+
+
+if __name__ == "__main__":
+    read = sys.stdin.readline
+    y, m, d = map(int, read().split())
+    ty, tm, td = map(int, read().split())
+
+    start_day = datetime.date(y, m, d)
+    start_after_1000 = datetime.date(y + 1000, m, d)
+    end_day = datetime.date(ty, tm, td)
+
+    result_1000 = start_after_1000 - start_day
+    result = end_day - start_day
+    if result < result_1000:
+        print("D-{}".format(result.days))
+    else:
+        print("gg")
