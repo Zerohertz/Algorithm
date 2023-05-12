6ebfde14be423a7866096e06acf0612cc08f406b
Zerohertz <ohg3417@gmail.com>
2023-05-12 14:22:17 +0900

    :computer: Add: BOJ 1193

---
+++ BOJ/Silver/1193/main.py
@@ -0,0 +1,29 @@
+import sys
+import math
+
+
+def solution(X):
+    if X == 1:
+        print('1/1')
+    else:
+        est_n = int(math.sqrt(2*X))
+        if est_n * (est_n - 1) < 2*X <=est_n * (est_n + 1):
+            n = est_n
+        else:
+            est_n += 1
+            n = est_n
+        m = n * (n + 1) // 2
+        if n % 2 == 0:
+            x, y = n, 1
+            x -= m - X
+            y += m - X
+        else:
+            x, y = 1, n
+            x += m - X
+            y -= m - X
+        print(str(x)+'/'+str(y))
+
+if __name__ == "__main__":
+    read = sys.stdin.readline
+    X = int(read())
+    solution(X)
