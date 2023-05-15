65e95de47a50f1315e9e9339d217498c280a4678
Zerohertz <ohg3417@gmail.com>
2023-05-15 09:26:11 +0900

    :computer: Add: BOJ 2565

---
+++ BOJ/Gold/2565/main.py
@@ -0,0 +1,18 @@
+import sys
+
+
+if __name__ == "__main__":
+    read = sys.stdin.readline
+    N = int(read())
+    l = [[0, 0] for _ in range(N)]
+    dp = [0 for _ in range(N)]
+    for i in range(N):
+        a, b = map(int, read().split())
+        l[i] = [a, b]
+    l.sort(key=lambda x: x[0])
+    for i in range(N):
+        for j in range(i):
+            if l[i][1] > l[j][1] and dp[i] < dp[j]:
+                dp[i] = dp[j]
+        dp[i] += 1
+    print(N - max(dp))
