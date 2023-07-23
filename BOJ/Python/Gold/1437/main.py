aff20d4ef70959a0ff85f1c1eec9113362c2530a
Zerohertz <ohg3417@gmail.com>
2023-07-23 16:12:20 +0900

    :sob: Add: BOJ 1437

--- BOJ/Python/Gold/1437/main.py
+++ BOJ/Python/Gold/1437/main.py
@@ -1,14 +1,39 @@
 import sys
+from collections import deque
 
 read = sys.stdin.readline
 
+
+def man(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def BFS():
+    q = deque([(hx, hy)])
+    while q:
+        x, y = q.popleft()
+        if man(x, y, tx, ty) <= 1000:
+            print("happy")
+            return
+        for i in range(N):
+            if not visited[i]:
+                nx, ny = pyeon[i]
+                if man(x, y, nx, ny) <= 1000:
+                    q.append((nx, ny))
+                    visited[i] = True
+    print("sad")
+    return
+
+
 if __name__ == "__main__":
-    N = int(read().strip())
-    if N < 5:
-        print(N)
-    elif N % 3 == 0:
-        print(pow(3, N // 3, 10_007))
-    elif N % 3 == 1:
-        print((pow(3, (N - 4) // 3, 10_007) * 4) % 10_007)
-    else:
-        print((pow(3, (N - 2) // 3, 10_007) * 2) % 10_007)
+    T = int(read())
+    for _ in range(T):
+        N = int(read())
+        hx, hy = map(int, read().split())
+        pyeon = []
+        for _ in range(N):
+            x, y = map(int, read().split())
+            pyeon.append((x, y))
+        tx, ty = map(int, read().split())
+        visited = [False for _ in range(N + 1)]
+        BFS()
