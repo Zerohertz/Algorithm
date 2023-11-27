90e49c625060c3809b6f1ce4147f011d0bcb98fb
Zerohertz <ohg3417@gmail.com>
2023-11-27 22:40:09 +0900

    :hammer: Add: BOJ 14002

---
+++ BOJ/Python/Gold/14002/main.py
@@ -0,0 +1,23 @@
+import sys
+
+read = sys.stdin.readline
+
+N = int(read())
+A = list(map(int, read().split()))
+res = [1 for _ in range(N)]
+
+for i in range(N):
+    for j in range(i):
+        if A[i] > A[j]:
+            res[i] = max(res[i], res[j] + 1)
+
+M = max(res)
+print(M)
+
+subseq = []
+for i in range(N - 1, -1, -1):
+    if res[i] == M:
+        subseq.append(A[i])
+        M -= 1
+
+print(*subseq[::-1])
