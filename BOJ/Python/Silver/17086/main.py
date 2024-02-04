a41e7fa683b8befb846d6c4e91577d369df6b650
Zerohertz <ohg3417@gmail.com>
2024-02-04 23:36:50 +0900

    :hammer: Update: BOJ 17086

--- BOJ/Python/Silver/17086/main.py
+++ BOJ/Python/Silver/17086/main.py
@@ -3,35 +3,31 @@ from collections import deque
 
 read = sys.stdin.readline
 
+dx = [1, -1, -1, 1, 1, 0, -1, 0]
+dy = [1, 1, -1, -1, 0, 1, 0, -1]
 
-def BFS(i, j):
-    dx = [1, -1, -1, 1, 1, 0, -1, 0]
-    dy = [1, 1, -1, -1, 0, 1, 0, -1]
-    visited = [[False for _ in range(M)] for _ in range(N)]
-    visited[i][j] = True
-    queue = deque([(i, j, 0)])
+
+def BFS():
     while queue:
         x, y, cnt = queue.popleft()
         for i in range(8):
             nx, ny = x + dx[i], y + dy[i]
             if not (0 <= nx < N and 0 <= ny < M):
                 continue
-            if maps[nx][ny] == 1:
-                return cnt + 1
-            if not visited[nx][ny]:
-                visited[nx][ny] = True
+            if not maps[nx][ny]:
+                maps[nx][ny] = 1
                 queue.append((nx, ny, cnt + 1))
+    return cnt
 
 
 if __name__ == "__main__":
     N, M = map(int, read().split())
     maps = []
-    for _ in range(N):
-        maps.append(list(map(int, read().split())))
-    result = 0
+    queue = deque()
     for i in range(N):
-        for j in range(M):
-            if maps[i][j] == 1:
-                continue
-            result = max(result, BFS(i, j))
-    print(result)
+        tmp = list(map(int, read().split()))
+        for j, value in enumerate(tmp):
+            if value == 1:
+                queue.append((i, j, 0))
+        maps.append(tmp)
+    print(BFS())
