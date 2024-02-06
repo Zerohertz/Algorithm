986abba1307b1ba0b8e7052b43bb2e1760ee6635
Zerohertz <ohg3417@gmail.com>
2024-02-06 11:31:24 +0900

    :hammer: Update: BOJ 1197

--- BOJ/Python/Gold/1197/main.py
+++ BOJ/Python/Gold/1197/main.py
@@ -3,6 +3,35 @@ import sys
 
 read = sys.stdin.readline
 
+if __name__ == "__main__":
+    V, E = map(int, read().split())
+    maps = [[] for _ in range(V)]
+    for _ in range(E):
+        A, B, C = map(int, read().split())
+        maps[A - 1].append((B - 1, C))
+        maps[B - 1].append((A - 1, C))
+    cnt = 1
+    results = 0
+    visited = [False for _ in range(V)]
+    paths = [(0, 0)]
+    while paths and cnt <= V:
+        weights, from_ = heapq.heappop(paths)
+        if visited[from_]:
+            continue
+        visited[from_] = True
+        cnt += 1
+        results += weights
+        for to_, weights in maps[from_]:
+            if not visited[to_]:
+                heapq.heappush(paths, (weights, to_))
+    print(results)
+
+"""
+import heapq
+import sys
+
+read = sys.stdin.readline
+
 V, E = map(int, read().split())
 G = [[] for _ in range(V + 1)]
 
@@ -28,3 +57,4 @@ while l:
             heapq.heappush(l, i)
 
 print(cost)
+"""
