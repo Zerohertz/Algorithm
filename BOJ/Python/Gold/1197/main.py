f9f33445e2adbd17ea23dfeb9c1eec50dc62be68
Zerohertz <ohg3417@gmail.com>
2024-02-08 21:55:55 +0900

    :hammer: Update: BOJ 1197

--- BOJ/Python/Gold/1197/main.py
+++ BOJ/Python/Gold/1197/main.py
@@ -3,6 +3,84 @@ import sys
 
 read = sys.stdin.readline
 
+
+class DistjointSet:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+
+    def find(self, node):
+        if node != self.parent[node]:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, node1, node2):
+        root1, root2 = self.find(node1), self.find(node2)
+        if root1 != root2:
+            self.parent[root2] = root1
+
+
+if __name__ == "__main__":
+    V, E = map(int, read().split())
+    maps = []
+    for _ in range(E):
+        A, B, C = map(int, read().split())
+        heapq.heappush(maps, (C, A - 1, B - 1))
+    results = 0
+    distjointset = DistjointSet(V)
+    cnt = 1
+    while maps:
+        cost, node1, node2 = heapq.heappop(maps)
+        if distjointset.find(node1) != distjointset.find(node2):
+            distjointset.union(node1, node2)
+            results += cost
+    print(results)
+
+
+"""3rd (Kruskal)
+import heapq
+import sys
+
+read = sys.stdin.readline
+
+
+class DistjointSet:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+
+    def find(self, node):
+        if node != self.parent[node]:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, node1, node2):
+        root1, root2 = self.find(node1), self.find(node2)
+        if root1 != root2:
+            self.parent[root2] = root1
+
+
+if __name__ == "__main__":
+    V, E = map(int, read().split())
+    maps = []
+    for _ in range(E):
+        A, B, C = map(int, read().split())
+        heapq.heappush(maps, (C, A - 1, B - 1))
+    results = 0
+    distjointset = DistjointSet(V)
+    cnt = 1
+    while maps:
+        cost, node1, node2 = heapq.heappop(maps)
+        if distjointset.find(node1) != distjointset.find(node2):
+            distjointset.union(node1, node2)
+            results += cost
+    print(results)
+"""
+
+"""2nd (Prim)
+import heapq
+import sys
+
+read = sys.stdin.readline
+
 if __name__ == "__main__":
     V, E = map(int, read().split())
     maps = [[] for _ in range(V)]
@@ -25,8 +103,10 @@ if __name__ == "__main__":
             if not visited[to_]:
                 heapq.heappush(paths, (weights, to_))
     print(results)
-
 """
+
+
+"""1st (Prim)
 import heapq
 import sys
 
