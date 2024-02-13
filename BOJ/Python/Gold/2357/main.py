2ac721833b01905ab144f0c718dd7eb359857960
Zerohertz <ohg3417@gmail.com>
2024-02-13 23:55:25 +0900

    :recycle: Refactor: BOJ 2357

--- BOJ/Python/Gold/2357/main.py
+++ BOJ/Python/Gold/2357/main.py
@@ -1,40 +1,53 @@
+import math
 import sys
 
 sys.setrecursionlimit(10**9)
 read = sys.stdin.readline
 
 
-def init(node, start, end):
-    if start == end:
-        tree[node] = (l[start], l[start])
-        return tree[node]
-    else:
-        x = init(node * 2, start, (start + end) // 2)
-        y = init(node * 2 + 1, (start + end) // 2 + 1, end)
-        tree[node] = (min(x[0], y[0]), max(x[1], y[1]))
-        return tree[node]
+class SegmentTree:
+    def __init__(self, values):
+        self.values = values
+        self.length = len(values)
+        size = math.ceil(math.log(self.length, 2) + 1e-9)
+        size = 1 << (size + 1)
+        self.tree = [0 for _ in range(size)]
+        self._init()
+
+    def _init(self, node=None, start=None, end=None):
+        if node is None and start is None and end is None:
+            node = 1
+            start = 0
+            end = self.length - 1
+        if start == end:
+            self.tree[node] = (self.values[start], self.values[start])
+            return self.tree[node]
+        value1 = self._init(node * 2, start, (start + end) // 2)
+        value2 = self._init(node * 2 + 1, (start + end) // 2 + 1, end)
+        self.tree[node] = (min(value1[0], value2[0]), max(value1[1], value2[1]))
+        return self.tree[node]
+
+    def minmax(self, left, right, node=None, start=None, end=None):
+        if node is None and start is None and end is None:
+            node = 1
+            start = 0
+            end = self.length - 1
+        if left > end or right < start:
+            return (sys.maxsize, -sys.maxsize)
+        if left <= start and end <= right:
+            return self.tree[node]
+        value1 = self.minmax(left, right, node * 2, start, (start + end) // 2)
+        value2 = self.minmax(left, right, node * 2 + 1, (start + end) // 2 + 1, end)
+        return (min(value1[0], value2[0]), max(value1[1], value2[1]))
 
 
-def segMinMax(node, start, end, left, right):
-    if left > end or right < start:
-        return (sys.maxsize, -sys.maxsize)
-    if left <= start and end <= right:
-        return tree[node]
-    x = segMinMax(node * 2, start, (start + end) // 2, left, right)
-    y = segMinMax(node * 2 + 1, (start + end) // 2 + 1, end, left, right)
-    return (min(x[0], y[0]), max(x[1], y[1]))
-
-
-N, M = map(int, read().split())
-
-l = [0 for _ in range(N)]
-for i in range(N):
-    l[i] = int(read())
-
-tree = [(sys.maxsize, -sys.maxsize) for _ in range(400_000)]
-init(1, 0, N - 1)
-
-for _ in range(M):
-    a, b = map(int, read().split())
-    a, b = segMinMax(1, 0, N - 1, a - 1, b - 1)
-    print(a, b)
+if __name__ == "__main__":
+    N, M = map(int, read().split())
+    l = [0 for _ in range(N)]
+    for i in range(N):
+        l[i] = int(read())
+    st = SegmentTree(l)
+    for _ in range(M):
+        a, b = map(int, read().split())
+        a, b = st.minmax(a - 1, b - 1)
+        print(a, b)
