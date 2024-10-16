578c116941ea02ca095324d069fbe26129dfe9a4
Zerohertz <ohg3417@gmail.com>
2024-10-16 16:02:31 +0900

    :memo: update: BOJ 1920

--- BOJ/Python/Silver/1920/main.py
+++ BOJ/Python/Silver/1920/main.py
@@ -1,3 +1,34 @@
+import sys
+
+read = sys.stdin.readline
+
+
+def bisect(b):
+    left, right = 0, N - 1
+    while left < right:
+        mid = (left + right) // 2
+        if A[mid] < b:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+
+
+def main():
+    A.sort()
+    for b in B:
+        print(int(A[bisect(b)] == b))
+
+
+if __name__ == "__main__":
+    N = int(read())
+    A = list(map(int, read().split()))
+    M = int(read())
+    B = list(map(int, read().split()))
+    main()
+
+
+"""1st
 def binary(A, n, tar):
     left = 0
     right = n - 1
@@ -21,3 +52,4 @@ for i in range(M):
         print(1)
     else:
         print(0)
+"""
