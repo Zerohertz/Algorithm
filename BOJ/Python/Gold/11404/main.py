51d02f3e5c7ed8fdae7dab4383164f6f522ba648
Zerohertz <ohg3417@gmail.com>
2024-11-11 23:44:16 +0900

    :sparkles: update: BOJ 11404

--- BOJ/Python/Gold/11404/main.py
+++ BOJ/Python/Gold/11404/main.py
@@ -2,6 +2,42 @@ import sys
 
 read = sys.stdin.readline
 
+INF = sys.maxsize
+
+
+def main():
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                if graph[i][j] <= graph[i][k] + graph[k][j]:
+                    continue
+                graph[i][j] = graph[i][k] + graph[k][j]
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] != INF:
+                continue
+            graph[i][j] = 0
+    for g in graph:
+        print(*g)
+
+
+if __name__ == "__main__":
+    n = int(read())
+    m = int(read())
+    graph = [[INF for _ in range(n)] for _ in range(n)]
+    for _ in range(m):
+        a, b, c = map(int, read().split())
+        graph[a - 1][b - 1] = min(c, graph[a - 1][b - 1])
+    main()
+
+
+"""1st
+import sys
+
+read = sys.stdin.readline
+
 n = int(read())
 m = int(read())
 
@@ -25,3 +61,4 @@ for i in res:
         else:
             print(j, end=" ")
     print()
+"""
