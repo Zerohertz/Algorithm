925eea3ab95c9f9317fb0538d583d7c243aee51d
Zerohertz <ohg3417@gmail.com>
2024-11-12 00:10:29 +0900

    :sparkles: update: BOJ 1916

--- BOJ/Python/Gold/1916/main.py
+++ BOJ/Python/Gold/1916/main.py
@@ -3,6 +3,42 @@ import sys
 
 read = sys.stdin.readline
 
+INF = sys.maxsize
+
+
+def main():
+    weights = [INF for _ in range(N)]
+    weights[start - 1] = 0
+    queue = [(0, start - 1)]
+    while queue:
+        w, v = heapq.heappop(queue)
+        if weights[v] < w:
+            continue
+        for _w, _v in graph[v]:
+            if weights[_v] <= w + _w:
+                continue
+            weights[_v] = w + _w
+            heapq.heappush(queue, (w + _w, _v))
+    print(weights[end - 1])
+
+
+if __name__ == "__main__":
+    N = int(read())
+    M = int(read())
+    graph = [[] for _ in range(N)]
+    for _ in range(M):
+        u, v, w = map(int, read().split())
+        graph[u - 1].append((w, v - 1))
+    start, end = map(int, read().split())
+    main()
+
+
+"""1st
+import heapq
+import sys
+
+read = sys.stdin.readline
+
 N = int(read())
 M = int(read())
 h = []
@@ -29,3 +65,4 @@ while h:
             heapq.heappush(h, (nextw, nv))
 
 print(d[end - 1])
+"""
