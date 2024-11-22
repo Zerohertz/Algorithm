eca35f26d85662289c848698c9d1385236290095
Zerohertz <ohg3417@gmail.com>
2024-11-22 10:24:27 +0900

    :sparkles: update: BOJ 11657

--- BOJ/Python/Gold/11657/main.py
+++ BOJ/Python/Gold/11657/main.py
@@ -2,6 +2,45 @@ import sys
 
 read = sys.stdin.readline
 
+INF = sys.maxsize
+
+
+def main():
+    distance = [INF for _ in range(N)]
+    distance[0] = 0
+    for _ in range(N - 1):
+        for node in range(N):
+            for _node, _dist in graph[node]:
+                if distance[node] == INF or distance[_node] <= distance[node] + _dist:
+                    continue
+                distance[_node] = distance[node] + _dist
+    for node in range(N):
+        for _node, _dist in graph[node]:
+            if distance[node] == INF or distance[_node] <= distance[node] + _dist:
+                continue
+            print(-1)
+            return
+    for node in range(1, N):
+        if distance[node] == INF:
+            print(-1)
+        else:
+            print(distance[node])
+    return
+
+
+if __name__ == "__main__":
+    N, M = map(int, read().split())
+    graph = [[] for _ in range(N)]
+    for _ in range(M):
+        A, B, C = map(int, read().split())
+        graph[A - 1].append((B - 1, C))
+    main()
+
+"""1st
+import sys
+
+read = sys.stdin.readline
+
 
 if __name__ == "__main__":
     N, M = map(int, read().split())
@@ -32,3 +71,4 @@ if __name__ == "__main__":
             print(-1)
         else:
             print(distance[i])
+"""
