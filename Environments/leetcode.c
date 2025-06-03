3876cfa6cd7dbaa53a6ba0acaefb5548a2790bb1
Zerohertz <ohg3417@gmail.com>
2025-06-03 21:47:22 +0900

    :hammer: update: clang
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

---
+++ Environments/leetcode.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+
+#include "stdio.h"
+
+bool solution() { return 0; }
+
+int main(int argc, char* argv[]) {
+  bool result = solution();
+  printf("%d", result);
+  return 0;
+}
--- Environments/leetcode.cpp
+++ Environments/leetcode.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Solution {
-public:
+ public:
   bool solution(int x) {
     if (x) {
       return false;
@@ -11,8 +11,8 @@ public:
   }
 };
 
-int main(int argc, char *argv[]) {
-  Solution *solution = new Solution();
+int main(int argc, char* argv[]) {
+  Solution* solution = new Solution();
   bool result = solution->solution(0);
   cout << result;
   return 0;
