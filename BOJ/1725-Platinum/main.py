3d65f9a77f400515c70dd79544237ce44824afd9
Zerohertz <ohg3417@gmail.com>
2025-06-03 22:35:14 +0900

    :recycle: refactor: boj
    ```python
    import os
    import shutil
    from glob import glob
    
    from tqdm import tqdm
    
    tmps = glob("*/*/*/*.py") + glob("*/*/*/*.go") + glob("*/*/*/*.java")
    for tmp in tqdm(tmps):
        lang, tier, num, file = tmp.split("/")
        path = os.path.join("tmp", f"{num}-{tier}")
        os.makedirs(path, exist_ok=True)
        shutil.copy(tmp, path)
    
    for tmp in tqdm(glob("*/*/*/Legacy")):
        lang, tier, num, file = tmp.split("/")
        path = os.path.join("tmp", f"{num}-{tier}", file)
        shutil.copytree(tmp, path)
    ```
    
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- BOJ/1725-Platinum/main.py
+++ BOJ/1725-Platinum/main.py
@@ -1,6 +1,45 @@
 import sys
 
 read = sys.stdin.readline
+
+if __name__ == "__main__":
+    N = int(read())
+    values = [0 for _ in range(N)]
+    for i in range(N):
+        values[i] = int(read())
+    stack = []
+    result = 0
+    for i in range(N + 1):
+        while stack and (i == N or values[stack[-1]] > values[i]):
+            height = values[stack[-1]]
+            stack.pop()
+            width = i
+            if stack:
+                width = i - stack[-1] - 1
+            result = max(result, width * height)
+        stack.append(i)
+    print(result)
+
+"""
+1. Segment Tree
+아래와 같이 처음 한번 통과했지만, 그 이후로는 모두 시간 초과.
+
+제출 번호	아이디	문제	결과	메모리	시간	언어	코드 길이	제출한 시간
+73379180	zerohertz	 1725	시간 초과			Python 3 / 수정	1702	1년 전
+73379145	zerohertz	 1725	시간 초과			Python 3 / 수정	1701	1년 전
+73379098	zerohertz	 1725	시간 초과			Python 3 / 수정	1776	1년 전
+73379063	zerohertz	 1725	시간 초과			Python 3 / 수정	1783	1년 전
+73379022	zerohertz	 1725	시간 초과			Python 3 / 수정	1769	1년 전
+73379002	zerohertz	 1725	메모리 초과			Python 3 / 수정	1772	1년 전
+73378944	zerohertz	 1725	시간 초과			Python 3 / 수정	1704	1년 전
+73378891	zerohertz	 1725	시간 초과			Python 3 / 수정	2471	1년 전
+73378879	zerohertz	 1725	런타임 에러 (IndexError)			Python 3 / 수정	2468	1년 전
+73378856	zerohertz	 1725	시간 초과			Python 3 / 수정	2307	1년 전
+73378764	zerohertz	 1725	맞았습니다!!	62892	3384	Python 3 / 수정	1702	1년 전
+
+import sys
+
+read = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 
@@ -65,3 +104,4 @@ if __name__ == "__main__":
     tree = [0 for _ in range(2**size)]
     _init(1, 0, length - 1)
     print(large(0, length - 1))
+"""
